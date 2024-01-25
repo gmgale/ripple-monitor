44341c046eb5cab51f701c79d9ce864c124e2db9
George Gale <george.gale@specto.si>
2024-01-25 13:39:23 +0100

    Swap tx dto for type in controller

--- api/src/api/api.controller.ts
+++ api/src/api/api.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+type Transaction = {
+  tx_id: number;
+  hash: string;
+  ledger_index: number;
+  timestamp: Date;
+  amount: number;
+  sender_address: string;
+  receiver_address: string;
+};
 
 @Controller('api')
-export class ApiController {}
+export class ApiController {
+  @Post()
+  createTransaction(@Body() transaction: Transaction) {
+    // Handle the transaction here
+    console.log(transaction);
+  }
+}
