7c8da775cf7c1d8d8c4e102597481f332edc0455
George Gale <george.gale@specto.si>
2024-01-29 20:43:41 +0100

    Add endpoints for new wallets

--- api/src/api/api.controller.ts
+++ api/src/api/api.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
 import { ApiKeyGuard } from '../guards/ApiKeyGuard';
-import { Transaction } from './transaction';
 import { ApiService } from './api.service';
+import { db } from '../db';
 
 @Controller('api')
 export class ApiController {
-  @Post()
+  @Post('add-transaction')
   @UseGuards(ApiKeyGuard)
-  createTransaction(@Body() transaction: Transaction) {
+  createTransaction(@Body() transaction: unknown) {
     // Check the transaction matches the expected format
     if (ApiService.isTransaction(transaction)) {
       // Handle the transaction here
@@ -15,6 +15,22 @@ export class ApiController {
     } else {
       // Handle the error here
       Logger.error('Invalid transaction');
+    }
+  }
+
+  @Post('add-wallet')
+  @UseGuards(ApiKeyGuard)
+  async addWallet(@Body() wallet: unknown) {
+    // Check the transaction matches the expected format
+    if (ApiService.isWallet(wallet)) {
+      // Handle the transaction here
+      await db.wallets.add(wallet);
+
+      // Send to the listener service
+      ApiService.listenToNewWallet(wallet);
+    } else {
+      // Handle the error here
+      Logger.error('Invalid wallet');
     }
   }
 }
--- api/src/api/api.service.ts
+++ api/src/api/api.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Transaction } from './transaction';
-import { db } from '../../db';
+import { db } from '../db';
+import { Wallet } from './wallet';
+import process from 'process';
+import axios from 'axios';
 
 @Injectable()
 export class ApiService {
@@ -25,6 +28,42 @@ export class ApiService {
         Logger.log(data.tx_id); // print new transaction id;
       })
       .catch((error) => {
+        Logger.log(error);
+      });
+  }
+
+  static isWallet(wallet: any): wallet is Wallet {
+    return wallet && typeof wallet === 'object' && 'address' in wallet;
+  }
+
+  static listenToNewWallet(wallet: Wallet) {
+    const data = JSON.stringify({
+      address: wallet.address,
+    });
+
+    const listenerUrl = process.env.LISTENER_URL;
+
+    if (!listenerUrl) {
+      throw Error('Missing environment variable LISTENER_URL');
+    }
+
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: listenerUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': process.env.API_KEY,
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response: { data: any }) => {
+        Logger.log(response.data);
+      })
+      .catch((error: any) => {
         Logger.log(error);
       });
   }
